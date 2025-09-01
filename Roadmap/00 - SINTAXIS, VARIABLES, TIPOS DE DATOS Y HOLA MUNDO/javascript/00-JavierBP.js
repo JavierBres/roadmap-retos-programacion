11cfd7d1cc8de4420c2d273b83f57df6980415aa
Javier BP <fjavierbresperez@yahoo.es>
2025-09-01 17:46:42 +0200

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/00-JavierBP.js
@@ -0,0 +1,35 @@
+// EJERCICIO:
+//  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+//  *   lenguaje de programación que has seleccionado.
+
+// Este es un comentario utilizando el lenguaje de programación JavaScript.
+// Estas son las dos URLs de la página de referencia para el lenguaje de programación JavaScript.
+// https://tc39.es/ecma262
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//  * - Representa las diferentes sintaxis que existen de crear comentarios
+//  *   en el lenguaje (en una línea, varias...).
+
+// Este es un comentario en una sola línea.
+/* Y este es un comentario 
+en varia s líneas.*/
+
+//  * - Crea una variable (y una constante si el lenguaje lo soporta).
+
+let variable = "Hola";
+const constante = "Hola";
+
+//  * - Crea variables representando todos los tipos de datos primitivos
+//  *   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+let string = "Hello!"
+let number = 73
+let boolean = true
+let undefined = undefined
+let variableNull = null
+let variableSymbol = Symbol("Hola")
+let bigInt = 23232432343524324n
+
+//  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+console.log("¡Hola, JavaScript!");
